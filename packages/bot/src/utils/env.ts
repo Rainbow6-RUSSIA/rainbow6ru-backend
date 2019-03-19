6692d53044bbd88d924102f5bc43be771163aca9
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-03-19 04:58:16 +0300

    env checks var

--- packages/bot/src/utils/env.ts
+++ packages/bot/src/utils/env.ts
@@ -1,14 +1,12 @@
 class IEnv {
     public PREFIX = '';
     public OWNERS = '';
-    public HOME_GUILD = '';
     public DISCORD_TOKEN = '';
     public DISCORD_SECRET = '';
     public CALLBACK_URL = '';
     public DB = '';
     public R6API_LOGIN = '';
     public R6API_PASSWORD = '';
-    public R6API_LOGLEVEL = '';
     public PACK_SIZE = '';
     // public MIGRATE = '';
     public COOLDOWN = '';
@@ -24,7 +22,7 @@ class IDefaultEnv extends IEnv {
 }
 
 for (const key in new IEnv()) {
-    if (!process.env[key]) { throw new Error(`Enviromental variable ${key} not specified`); }
+    if (!process.env[key] && process.env.ENV_CHECK) { throw new Error(`Enviromental variable ${key} not specified`); }
 }
 
 export default process.env as any as IDefaultEnv;
