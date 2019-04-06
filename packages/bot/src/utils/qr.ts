29802d957963ee4ec856c208a2de45b66542be14
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-04-07 00:09:58 +0300

    png parsing fixed

--- packages/bot/src/utils/qr.ts
+++ packages/bot/src/utils/qr.ts
@@ -17,17 +17,7 @@ export function generate(genome: UUID, id: string): Promise<Uint8Array> {
             backgroundImage: __dirname + '/../../assets/r6rus.png',
             borderDark : 'rgba(0, 0, 0, .1)',
             borderLight : 'rgba(255, 255, 255, .1)',
-            callback: (data) => {
-                // binary PNG data
-                if (data) {
-                    resolve(data);
-                } else {
-                    reject(new Error('QR No Data'));
-                }
-                    // fs.writeFile('test.png', data, 'binary', function(err) {
-                    //     console.log(err);
-                    // });
-            },
+            callback: (data) => data ? resolve(data) : reject(new Error('QR No Data')),
             colorDark: 'rgba(0, 0, 0, 0.8)',
             colorLight: 'rgba(255, 255, 255, 1)',
             correctLevel: QRErrorCorrectLevel.H,
@@ -36,7 +26,7 @@ export function generate(genome: UUID, id: string): Promise<Uint8Array> {
             logoCornerRadius: 8,
             margin: 5,
             size: 500,
-            text: `${genome}_${id}_${createHash('md5').update(`${genome}_${id}_${ENV.KEY256}`).digest('base64')}`,
+            text: `${id}${createHash('md5').update(`${genome}_${id}_${ENV.KEY256}`).digest('base64')}`,
             typeNumber: 4,
             whiteMargin: true,
         });
@@ -46,11 +36,12 @@ export function generate(genome: UUID, id: string): Promise<Uint8Array> {
 export async function verify(genome: UUID, id: string): Promise<boolean> {
     const res = await fetch(`https://ubisoft-avatars.akamaized.net/${genome}/default_256_256.png`);
     const buff = await res.buffer();
-    const img = await new Promise<PNG>((resolve, reject) => png.parse(buff, (err, data) => err ? reject(err) : resolve(data)));
+    const img = PNG.sync.read(buff);
+    // const img = await new Promise<PNG>((resolve, reject) => png.parse(buff, (err, data) => err ? reject(err) : resolve(data)));
     const code = readerQR(Uint8ClampedArray.from(img.data), img.width, img.height);
     if (code) {
-        const args = code.data.split('_');
-        return createHash('md5').update(`${args[0]}_${args[1]}_${ENV.KEY256}`).digest('base64') === args[2] && args[0] === genome && args[1] === id;
+        const args = [code.data.slice(0, 18), code.data.slice(18)];
+        return createHash('md5').update(`${genome}_${args[0]}_${ENV.KEY256}`).digest('base64') === args[1] && args[0] === id;
     } else {
         throw new Error('QR No Data');
     }
