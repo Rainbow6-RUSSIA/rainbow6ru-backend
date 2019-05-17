a1576489589d58d5723f5b3be85525cf907c06cc
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-05-18 01:34:09 +0300

    updated QR generator & reduced asset resolution

--- packages/bot/src/utils/image.ts
+++ packages/bot/src/utils/image.ts
@@ -5,22 +5,22 @@ Canvas.registerFont(__dirname + '/../../assets/BebasNeue Bold.otf', { family: 'B
 
 export async function createLobbyPreview(n: number, m: number, k: number = 0) {
     if (!(Number.isInteger(n) && Number.isInteger(m) && Number.isInteger(k))) { return; }
-    const preview = Canvas.createCanvas(650, 650);
+    const preview = Canvas.createCanvas(160, 160);
     const ctx = preview.getContext('2d');
-    const images = await Promise.all([Canvas.loadImage(__dirname + `/../../assets/ranks/${n/* Math.min(...lobby.members.map((m) => m.rank)) */}.png`), Canvas.loadImage(__dirname + `/../../assets/ranks/${m/* Math.max(...lobby.members.map((m) => m.rank)) */}.png`)]);
-    ctx.font = '200px "BebasNeue Bold"';
-    // ctx.fillStyle = 'red';
-    // ctx.fillRect(0, 0, preview.width, preview.height);
+    const images = await Promise.all([Canvas.loadImage(__dirname + `/../../assets/ranks/${n}.png`), Canvas.loadImage(__dirname + `/../../assets/ranks/${m}.png`)]);
+    ctx.font = '50px "BebasNeue Bold"';
     if (k > 0) {
         ctx.fillStyle = 'white';
-        ctx.fillText(`+${k}`, 20, 160);
+        ctx.fillText(`+${k}`, 5, 40);
     }
-    ctx.shadowOffsetX = -25;
-    ctx.shadowOffsetY = 20;
+    ctx.shadowOffsetX = -7;
+    ctx.shadowOffsetY = 7;
     ctx.shadowColor = 'black';
-    ctx.shadowBlur = 10;
-    ctx.drawImage(images[0], -50, 150);
-    ctx.drawImage(images[1], 200, 0);
-    return preview.toBuffer();
+    ctx.shadowBlur = 2.5;
+    ctx.drawImage(images[0], -12, 37);
+    ctx.drawImage(images[1], 50, 0);
     // writeFile(__dirname + `/../../assets/test.png`, preview.toBuffer(), console.log);
+    return preview.toBuffer();
 }
+
+// createLobbyPreview(18, 20, 4);
