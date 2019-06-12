679b00eebd53fd81fce889e07cb5a37a4a257706
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-06-12 11:53:18 +0300

    new rank colors & discovery parsing

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -107,7 +107,7 @@ export interface IActivityCounter {
 export enum IngameStatus {
     OTHER = -1,
     MENU = 0,
-    CASUAL, CASUAL_SEARCH, RANKED, RANKED_SEARCH, TERRORIST_HUNT, CUSTOM, CUSTOM_SEARCH
+    CASUAL, CASUAL_SEARCH, RANKED, RANKED_SEARCH, TERRORIST_HUNT, CUSTOM, CUSTOM_SEARCH, DISCOVERY, DISCOVERY_SEARCH
 }
 
 export const R6_PRESENCE_ID = '445956193924546560';
@@ -120,17 +120,19 @@ export const R6_PRESENCE_REGEXPS = [
     [/Поиск.+РЕЙТ/g, /Looking.+RANKED/g],
     [/Антитеррор/g, /TERRORIST HUNT/g],
     [/Игра.+ПОЛЬЗ/g],
-    [/Поиск.+ПОЛЬЗ/g]
+    [/Поиск.+ПОЛЬЗ/g],
+    [/РАЗВЕДКА.+раунд/g,/DISCOVERY.+Round/g],
+    [/Поиск.+РАЗВЕДКА/g, /Looking.+DISCOVERY/g]
 ];
 
 export const RANK_COLORS = [
-    2301728,
-    8137472,8137472,8137472,8137472,
-    12351548,12351548,12351548,12351548,
-    10855845,10855845,10855845,10855845,
-    15576885,15576885,15576885,15576885,
-    15586990,15586990,15586990,
-    7977902
+    0x231f20,
+    0x7c2b00,0x7c2b00,0x7c2b00,0x7c2b00,
+    0xbc783c,0xbc783c,0xbc783c,0xbc783c,
+    0xa5a5a5,0xa5a5a5,0xa5a5a5,0xa5a5a5,
+    0xedaf35,0xedaf35,0xedaf35,0xedaf35,
+    0xb6ffff,0xb6ffff,0xb6ffff,
+    0xc07bde
 ];
 
 export type DonateRecord = [Snowflake, number, string, Snowflake[]]
