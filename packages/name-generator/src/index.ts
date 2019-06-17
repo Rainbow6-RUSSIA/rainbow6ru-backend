5884c55b46ba0a4ff8686ceb08b379e8d94bbec6
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-06-17 16:07:39 +0300

    name-generator
    Co-authored-by: Daniil <d.a.zhukov22@gmail.com>

---
+++ packages/name-generator/src/index.ts
@@ -0,0 +1,38 @@
+const names = [
+    'ATMs', 'Anchor', 'Antechamber', 'Aquarium',
+    'Archives', 'Armory', 'Attic', 'BBQ',
+    'Backyard', 'Bakery', 'Balcony', 'Bar',
+    'Barn', 'Barnyard', 'Bathroom', 'Bedroom',
+    'Boulevard', 'Brewery', 'Bridge', 'Bunk',
+    'Bunker', 'Cabin', 'Cabinet', 'Cafe',
+    'Cafeteria', 'Campfire', 'Camping', 'Cantina',
+    'Casino', 'Caterer', 'Chapel', 'Church',
+    'Classroom', 'Clearance', 'Cliffside', 'Closet',
+    'Cockpit', 'Container', 'Courtyard', 'Crypt',
+    'Depot', 'Detention', 'Dormitory', 'Dorms',
+    'Driveway', 'Dumpster', 'Elevator', 'Elevators',
+    'Engine', 'Exhibition', 'Farmlands', 'Festival',
+    'Fountain', 'Foyer', 'Gallery', 'Garage',
+    'Garden', 'Gardens', 'Gargoyle', 'Garrage',
+    'Gazeebo', 'Greenhouse', 'Gym', 'Hallway',
+    'Hammam', 'Helipad', 'Infirmary', 'Jacuzzi',
+    'Junkyard', 'Karaoke', 'Kennels', 'Kitchen',
+    'Lakeside', 'Landing', 'Laundry', 'Library',
+    'Lobby', 'Lockers', 'Lockgate', 'Lounge',
+    'Market', 'Mezzanine', 'Mudroom', 'Museum',
+    'Office', 'Palms', 'Pantry', 'Park',
+    'Parking', 'Patio', 'Penthouse', 'Pergola',
+    'Pipes', 'Plaza', 'Pool', 'Radio',
+    'Reception', 'Restaurant', 'Roundabout', 'Sailboats',
+    'Sewer', 'Shop', 'Showers', 'Snowmobiles',
+    'Stable', 'Storage', 'Street', 'Study',
+    'Submarine', 'Taiko', 'Teacups', 'Tellers',
+    'Terrace', 'Theater', 'Tower', 'Valley',
+    'Vault', 'Ventilation', 'Veranda', 'Village',
+    'Walk-in', 'Walkway', 'Warehouse', 'Washrooms',
+    'Workshop', 'Zodiac',
+];
+
+export default function generate() {
+    return names[Math.round(Math.random() * names.length)];
+}
