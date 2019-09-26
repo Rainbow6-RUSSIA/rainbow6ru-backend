4eed1ea976dbc8963c546fa5399155b7ac3fda6b
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-09-27 00:18:28 +0300

    new modes

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -108,7 +108,7 @@ export enum IngameStatus {
     LOADING = -2,
     OTHER = -1,
     MENU = 0,
-    CASUAL, CASUAL_SEARCH, RANKED, RANKED_SEARCH, NEWCOMER, NEWCOMER_SEARCH, TERRORIST_HUNT, CUSTOM, CUSTOM_SEARCH, DISCOVERY, DISCOVERY_SEARCH,
+    CASUAL, CASUAL_SEARCH, UNRANKED, UNRANKED_SEARCH, RANKED, RANKED_SEARCH, NEWCOMER, NEWCOMER_SEARCH, TERRORIST_HUNT, CUSTOM, CUSTOM_SEARCH, DISCOVERY, DISCOVERY_SEARCH,
 }
 
 export const R6_PRESENCE_ID = '445956193924546560';
@@ -117,35 +117,39 @@ export const R6_PRESENCE_REGEXPS = [
     [/МЕНЮ/g, /MENU/g],
     [/ОБЫЧНАЯ.+раунд/g, /CASUAL.+Round/g],
     [/Поиск.+ОБЫЧНАЯ/g, /Looking.+CASUAL/g],
+    [/БЕЗРЕЙТ.+раунд/g, /UNRANKED.+Round/g],
+    [/Поиск.+БЕЗРЕЙТ/g, /Looking.+UNRANKED/g],
     [/РЕЙТ.+раунд/g, /RANKED.+Round/g],
     [/Поиск.+РЕЙТ/g, /Looking.+RANKED/g],
     [/НОВИЧОК.+раунд/g, /NEWCOMER.+Round/g],
     [/Поиск.+НОВИЧОК/g, /Looking.+NEWCOMER/g],
     [/Антитеррор/g, /TERRORIST HUNT/g],
-    [/Игра.+ПОЛЬЗ/g],
-    [/Поиск.+ПОЛЬЗ/g],
+    [/Игра.+ПОЛЬЗ/g, /CUSTOM.+Round/g],
+    [/Поиск.+ПОЛЬЗ/g, /Looking.+CUSTOM/g],
     [/РАЗВЕДКА.+раунд/g, /DISCOVERY.+Round/g],
     [/Поиск.+РАЗВЕДКА/g, /Looking.+DISCOVERY/g],
 ];
 
 export const RANK_COLORS = [
-    0x231f20,
-    0x7c2b00, 0x7c2b00, 0x7c2b00, 0x7c2b00,
-    0xbc783c, 0xbc783c, 0xbc783c, 0xbc783c,
-    0xa5a5a5, 0xa5a5a5, 0xa5a5a5, 0xa5a5a5,
-    0xedaf35, 0xedaf35, 0xedaf35, 0xedaf35,
-    0xb6ffff, 0xb6ffff, 0xb6ffff,
-    0xc07bde,
+    0x1f191a,
+    0x902700, 0x902700, 0x902700, 0x902700, 0x902700,
+    0xd17935, 0xd17935, 0xd17935, 0xd17935, 0xd17935,
+    0xa7a7a7, 0xa7a7a7, 0xa7a7a7, 0xa7a7a7, 0xa7a7a7,
+    0xffb120, 0xffb120, 0xffb120,
+    0x00cdc1, 0x00cdc1, 0x00cdc1,
+    0xa67df8,
+    0xff0784,
 ];
 
 export const RANK_BADGES = [
-    '<:rank0:598137242829062154>',
-    '<:rank1:598137242451443743>', '<:rank2:598137243260944394>', '<:rank3:598137243043102720>', '<:rank4:598137243001159680>',
-    '<:rank5:598137242946371584>', '<:rank6:598137242955022347>', '<:rank7:598137244100067329>', '<:rank8:598137243001159681>',
-    '<:rank9:598137243529641994>', '<:rank10:598137242996703235>', '<:rank11:598137242996703234>', '<:rank12:598137243193966592>',
-    '<:rank13:598137243391229962>', '<:rank14:598137243206418432>', '<:rank15:598137243659665408>', '<:rank16:598137243349024768>',
-    '<:rank17:598137243206418462>', '<:rank18:598137243013742593>', '<:rank19:598137243336704030>',
-    '<:rank20:598137243277721629>',
+    '598137242829062154',
+    '622165779328270367', '598137242451443743', '598137243260944394', '598137243043102720', '598137243001159680',
+    '622165779843907584', '598137242946371584', '598137242955022347', '598137244100067329', '598137243001159681',
+    '622165779990839318', '598137243529641994', '598137242996703235', '598137242996703234', '598137243193966592',
+    '598137243206418432', '598137243659665408', '598137243349024768',
+    '598137243206418462', '598137243013742593', '598137243336704030',
+    '598137243277721629',
+    '622159827681935371',
 ];
 
 export const EMOJI_REGEXP = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
