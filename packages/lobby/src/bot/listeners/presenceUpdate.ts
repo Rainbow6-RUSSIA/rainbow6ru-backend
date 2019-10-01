3e622f5352b0a8b5d660a74d3ac8385f11b1f47f
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-10-02 00:13:54 +0300

    feat(@r6ru/lobby, status): implement status observing

--- packages/lobby/src/bot/listeners/presenceUpdate.ts
+++ packages/lobby/src/bot/listeners/presenceUpdate.ts
@@ -1,6 +1,7 @@
 import { IngameStatus as IS, R6_PRESENCE_ID, R6_PRESENCE_REGEXPS } from '@r6ru/types';
 import { Listener } from 'discord-akairo';
-import { Presence } from 'discord.js';
+import { Collection, Presence } from 'discord.js';
+import { debug } from '../..';
 import { lobbyStoresRooms } from '../../utils/lobby';
 
 const detectIngameStatus = (presence: Presence): IS  => {
@@ -11,6 +12,8 @@ const detectIngameStatus = (presence: Presence): IS  => {
         return IS.OTHER;
     }
 };
+
+const start = [[IS.CASUAL_SEARCH, IS.CASUAL], [IS.RANKED_SEARCH, IS.RANKED], [IS.NEWCOMER_SEARCH, IS.NEWCOMER], [IS.DISCOVERY_SEARCH, IS.DISCOVERY], [IS.UNRANKED_SEARCH, IS.UNRANKED]];
 
 export default class PresenceUpdate extends Listener {
     public constructor() {
@@ -24,42 +27,40 @@ export default class PresenceUpdate extends Listener {
         const room = lobbyStoresRooms.get(newPresence.member.voice.channelID);
         if (room && room.status !== IS.LOADING) {
             console.log('PROCESS STATUS');
-            const statuses = room.dcMembers.map(m => m.presence).map(detectIngameStatus);
-            const statusSet = new Set(statuses);
-            if (statusSet.size <= 2) {
-                Object.values(room.LS.guild.lobbySettings);
-                // move when playing incorrect mode
-            }
+            const statusColl = new Collection<IS, number>();
+            room.dcMembers.map(m => m.presence).map(detectIngameStatus).map(s => statusColl.set(s, (statusColl.get(s) || 0) + 1));
+            statusColl.sort((a, b, ak, bk) => (b - a) || (bk - ak)); // sort by quantity otherwise sort by mode from actual mode to OTHER
+            // if (statusColl.size <= 2 && statusColl.has(IS.OTHER)) {
+            //     Object.values(room.LS.guild.lobbySettings);
+            //     // move when playing incorrect mode
+            // }
+            const prevStatus = room.status;
+            const nextStatus = statusColl.firstKey();
+            if (prevStatus !== nextStatus) {
+                console.log(IS[prevStatus], '-->', IS[nextStatus], statusColl);
+                if (![prevStatus, nextStatus].includes(IS.OTHER)) {
+                    if (start.some(t => t[0] === prevStatus && t[1] === nextStatus)) {
+                        debug.log(`<@${room.members.map(m => m.id).join('>, <@')}> начали играть (\`${IS[prevStatus]} --> ${IS[nextStatus]}\`). ID пати \`${room.id}\``);
+                    }
 
+                    if (start.some(t => t[1] === prevStatus) && nextStatus === IS.MENU) {
+                        debug.log(`<@${room.members.map(m => m.id).join('>, <@')}> закончили играть (\`${IS[prevStatus]} --> ${IS[nextStatus]}\`). ID пати \`${room.id}\``);
+                    }
+
+                    if (prevStatus === IS.RANKED && nextStatus === IS.MENU) {
+                        room.members.map(m => {
+                            m.rankUpdatedAt = new Date('2000');
+                            m.save();
+                        });
+                    }
+                }
+
+                room.status = nextStatus;
+                await Promise.all([
+                    room.updateAppeal(),
+                    room.LS.updateFastAppeal(),
+                ]);
+            }
         }
     }
 }
-
-/*
-public refreshIngameStatus = async (lobby: LSRoom) => {
-        const statuses = lobby.dcMembers.map(m => LobbyStore.detectIngameStatus(m.presence)).filter(is => is !== IS.OTHER);
-        statuses.unshift(IS.OTHER);
-        const s = lobby.status;
-        lobby.status = statuses.reduce((acc, el) => {
-            acc.k[el] = acc.k[el] ? acc.k[el] + 1 : 1;
-            acc.max = acc.max ? acc.max < acc.k[el] ? el : acc.max : el;
-            return acc;
-          }, { k: {}, max: null }).max;
-        if (s !== lobby.status && ![s, lobby.status].includes(IS.OTHER)) {
-            const start = [[IS.CASUAL_SEARCH, IS.CASUAL], [IS.RANKED_SEARCH, IS.RANKED]];
-            const stop = [[IS.CASUAL, IS.MENU], [IS.RANKED, IS.MENU]];
-            if (stop.some(t => JSON.stringify(t) === JSON.stringify([s, lobby.status]))) {
-                debug.log(`<@${lobby.members.map(m => m.id).join('>, <@')}> закончили играть (\`${IS[s]} --> ${IS[lobby.status]}\`). ID пати \`${lobby.id}\``);
-            }
-            if (start.some(t => JSON.stringify(t) === JSON.stringify([s, lobby.status]))) {
-                debug.log(`<@${lobby.members.map(m => m.id).join('>, <@')}> начали играть (\`${IS[s]} --> ${IS[lobby.status]}\`). ID пати \`${lobby.id}\``);
-            }
-            if (JSON.stringify(stop[1]) === JSON.stringify([s, lobby.status])) {
-                lobby.members.forEach(m => {
-                    m.rankUpdatedAt = new Date('2000');
-                    m.save();
-                });
-            }
-        }
-    }
-*/
