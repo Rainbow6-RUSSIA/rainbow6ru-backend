e2b0dd8b549750ba33282ff96bf1fa49e547823e
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-01-21 00:52:45 +0300

    test(@r6ru/db): add test

--- packages/db/src/test.ts
+++ packages/db/src/test.ts
@@ -1,11 +1,18 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import db from './index';
+import db, { Lobby } from './index';
 import User from './models/User';
 
 (async () => {
-    await db(process.env.DB);
+    await db(process.env.DB, true);
+    console.log('[TEST] CONNECTED!');
     const user = await User.findByPk('125634283258773504');
-    console.log(user);
+    const lobby = await Lobby.findByPk(201726, { include: [{all: true}] });
+    // tslint:disable-next-line:no-debugger
+    debugger;
+    console.log(1, lobby);
+    console.log(2, lobby.toJSON());
+    console.log(3, lobby.get());
+    process.exit(0);
 })();
