ec5c051dfc817d7dc21170da3cd064c280b1b357
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-05-08 14:37:58 +0300

    PS4 nickame support

--- packages/bot/src/bot/types/ubiNickname.ts
+++ packages/bot/src/bot/types/ubiNickname.ts
@@ -2,6 +2,6 @@ import { Message } from 'discord.js';
 
 export default (message: Message, phrase: string): string | null => {
     if (!phrase) { return null; }
-    return /^[a-zA-Z][\w-.]{2,14}$/g.test(phrase) && !phrase.toLowerCase().startsWith('ubi_') && !phrase.toLowerCase().endsWith('_ubi')
+    return /^[a-zA-Z][\w-.]{2,16}$/g.test(phrase) && !phrase.toLowerCase().startsWith('ubi_') && !phrase.toLowerCase().endsWith('_ubi')
         ? phrase : null;
 };
