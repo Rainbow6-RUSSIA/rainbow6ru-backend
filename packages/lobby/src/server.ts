f09d5715d3a9baf2f100ba0a234d1e7038af1c91
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-10-19 02:30:13 +0300

    Create animated preview

--- packages/lobby/src/server.ts
+++ packages/lobby/src/server.ts
@@ -1,11 +1,12 @@
 import { Lobby } from '@r6ru/db';
 import { IngameStatus as IS } from '@r6ru/types';
+import { User } from 'discord.js';
 import * as restify from 'restify';
-import { BadRequestError, NotFoundError } from 'restify-errors';
+import { BadRequestError, NotFoundError, PaymentRequiredError } from 'restify-errors';
 import ENV from './utils/env';
 import { lobbyStoresRooms } from './utils/lobby';
 import { LSRoom } from './utils/lobby/room';
-import { createLobbyPreview } from './utils/preview';
+import { createEnhancedUserPreview, createLobbyPreview } from './utils/preview';
 
 function respond(req, res, next) {
     res.send('hello ' + req.params.name);
@@ -24,12 +25,12 @@ server.use(restify.plugins.requestLogger());
 
 server.get('/auth/login', respond);
 
-server.get('/lobby/:id/preview', async (req, res, next) => {
+const lobbyGetterMiddleware = async (req: restify.Request & { data?: LSRoom }, res: restify.Response, next: restify.Next) => {
   if (Number.isInteger(parseInt(req.params.id)) && parseInt(req.params.id) < 2 ** 32 / 2) {
     const lobby = await Lobby.findByPk(req.params.id);
 
     if (!lobby) {
-      return next(new NotFoundError());
+      return res.send(new NotFoundError());
     }
 
     const room = lobbyStoresRooms.get(lobby.channel);
@@ -37,33 +38,38 @@ server.get('/lobby/:id/preview', async (req, res, next) => {
     if (!room) {
       return res.redirect(301, 'https://i.imgur.com/5Neb9Sn.png', next);
     } else {
-      // await waitLoaded(room);
-      const pic = await createLobbyPreview(
-        room.minRank,
-        room.maxRank,
-        (!room.joinAllowed
-          ? room.dcChannel.userLimit - room.dcMembers.size
-          : 0));
-
-      return res.sendRaw(200, pic || 'Error', {
-        'Content-Disposition': `inline; filename="preview-${req.id().split('-')[0]}.png"`,
-        'Content-Type': 'image/png',
-      });
+      req.data = room;
+      next()
     }
 
   } else {
-    return next(new BadRequestError());
+    return res.send(new BadRequestError());
   }
+}
+
+server.get('/lobby/:id/leader', lobbyGetterMiddleware, async (req: restify.Request & { data: LSRoom }, res, next) => {
+  const room = req.data;
+
+  if (!room.isEnhanced) res.send(new PaymentRequiredError())
+  
+  createEnhancedUserPreview(room.dcLeader.user, res);
+  // createEnhancedUserPreview({ id: '261871531418845186', avatar: 'a_d89a473082eb25f2383e75e8e7d07d98' } as User, res);
+})
+
+server.get('/lobby/:id/preview', lobbyGetterMiddleware, async (req: restify.Request & { data: LSRoom }, res, next) => {
+  const room = req.data;
+  
+  const pic = await createLobbyPreview(
+    room.minRank,
+    room.maxRank,
+    (!room.joinAllowed
+      ? room.dcChannel.userLimit - room.dcMembers.size
+      : 0));
+
+  return res.sendRaw(200, pic || 'Error', {
+    'Content-Disposition': `inline; filename="preview-${req.id().split('-')[0]}.png"`,
+    'Content-Type': 'image/png',
+  });
 });
 
 server.listen(ENV.PORT || 3000, () => console.log(`[INFO][GENERIC] ${server.name} listening at ${server.url}`));
-
-// async function waitLoaded(room: LSRoom) {
-//   return new Promise(resolve => {
-//       const waiter = () => {
-//           if (room.status !== IS.LOADING) { return resolve(); }
-//           setTimeout(waiter, 25);
-//       };
-//       waiter();
-//   });
-// }
--- packages/lobby/src/utils/preview.ts
+++ packages/lobby/src/utils/preview.ts
@@ -1,4 +1,8 @@
+import { User } from 'discord.js';
 import * as Canvas from 'canvas';
+import fetch from 'node-fetch'
+import * as GIFEncoder from 'gifencoder';
+import * as restify from 'restify';
 
 Canvas.registerFont(__dirname + '/../../assets/BebasNeue Bold.otf', { family: 'BebasNeue Bold' });
 
@@ -23,6 +27,67 @@ export async function createLobbyPreview(n: number, m: number, k: number = 0) {
     ctx.drawImage(images[1], 50, 0);
     // writeFile(__dirname + `/../../assets/test.png`, preview.toBuffer(), console.log);
     return preview.toBuffer();
+}
+
+const rainbow = Canvas.createCanvas(320, 320);
+const rainbowCtx = rainbow.getContext('2d');
+
+var CX = rainbow.width / 2,
+    CY = rainbow.height/ 2,
+    sx = CX,
+    sy = CY;
+
+for(var i = 0; i < 360; i+=0.1){
+    var rad = i * (2*Math.PI) / 360;
+    rainbowCtx.strokeStyle = "hsla("+i+", 100%, 50%, 1.0)";   
+    rainbowCtx.beginPath();
+    rainbowCtx.moveTo(CX, CY);
+    rainbowCtx.lineTo(CX + sx * Math.cos(rad), CY + sy * Math.sin(rad));
+    rainbowCtx.stroke();
+}
+
+export async function createEnhancedUserPreview(user: User, res: restify.Response) {
+    
+    // if (user.avatar.startsWith('a_')) {
+    //     const res = await fetch(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.gif?size=128`);
+    //     res.body.pipe();
+
+    // } else {
+        // const avatar = await fetch().then(d => d.blob())
+    // }
+    const canvasAvatar = await Canvas.loadImage(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=128`, { width: 160, height: 160 })
+
+    const FRAMES = 10;
+    const BORDER = 3;
+
+    const encoder = new GIFEncoder(160, 160);
+    encoder.createReadStream().pipe(res);
+
+    encoder.start();
+    encoder.setTransparent(0x000000); // make black transparent
+    encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
+    encoder.setDelay(1000 / FRAMES); // frame delay in ms
+    encoder.setQuality(10); // image quality. 10 is default.
+
+    const preview = Canvas.createCanvas(160, 160);
+    const ctx = preview.getContext('2d');
+
+    const { width, height } = preview
+
+    ctx.translate(width / 2, height / 2);
+
+    for (let i = 0; i <= FRAMES; i++) {
+        const angle = 2 * Math.PI / FRAMES; 
+        ctx.rotate(angle * i);
+        ctx.drawImage(rainbow, -width, -height);
+        ctx.rotate(-angle * i);
+
+        ctx.drawImage(canvasAvatar, -width / 2 + BORDER, -height / 2 + BORDER, width - BORDER * 2, height - BORDER * 2);
+
+        encoder.addFrame(ctx);
+    }
+
+    encoder.finish();
 }
 
 // createLobbyPreview(18, 20, 4);
